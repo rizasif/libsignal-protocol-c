318a3de279439841ac8e67bc8a56216c8cb3264d
Rizwan Asif <rizasif@gmail.com>
2018-02-13 14:16:27 +0500

    Overide: initiate

--- src/Codistan/signal_protocol_overrides.c
+++ src/Codistan/signal_protocol_overrides.c
@@ -2,102 +2,135 @@
 
 #include "signal_protocol_internal.h"
 
-#include <CommonCrypto/CommonDigest.h>
-#include <CommonCrypto/CommonHMAC.h>
-#include <CommonCrypto/CommonCryptor.h>
+#include <openssl/opensslv.h>
+#include <openssl/evp.h>
+#include <openssl/hmac.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
 
 #include <stdio.h>
 
 int signal_crypto_random(signal_context *context, uint8_t *data, size_t len){
-    arc4random_buf(data, len);
-    return 0;
-
-#if 0
-    /*
-     * Apple's documentation recommends this method for generating secure
-     * random numbers. However, it is too slow for the purpose of unit tests.
-     */
-    int result = 0;
-
-    FILE *fp = fopen("/dev/random", "r");
-    if(!fp) {
-        result = SG_ERR_UNKNOWN;
-        goto complete;
+    if(RAND_bytes(data, len)) {
+        return 0;
     }
-
-    size_t n = fread(data, 1, len, fp);
-    if(n != len) {
-        result = SG_ERR_UNKNOWN;
-        goto complete;
+    else {
+        return SG_ERR_UNKNOWN;
     }
-
-complete:
-    if(fp) {
-        fclose(fp);
-    }
-    return result;
-#endif
 }
 
 int signal_hmac_sha256_init(signal_context *context, void **hmac_context, const uint8_t *key, size_t key_len){
-    CCHmacContext *ctx = malloc(sizeof(CCHmacContext));
+    #if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+    HMAC_CTX *ctx = HMAC_CTX_new();
     if(!ctx) {
         return SG_ERR_NOMEM;
     }
+#else
+    HMAC_CTX *ctx = malloc(sizeof(HMAC_CTX));
+    if(!ctx) {
+        return SG_ERR_NOMEM;
+    }
+    HMAC_CTX_init(ctx);
+#endif
 
-    CCHmacInit(ctx, kCCHmacAlgSHA256, key, key_len);
     *hmac_context = ctx;
+
+    if(HMAC_Init_ex(ctx, key, key_len, EVP_sha256(), 0) != 1) {
+        return SG_ERR_UNKNOWN;
+    }
 
     return 0;
 }
 
 int signal_hmac_sha256_update(signal_context *context, void *hmac_context, const uint8_t *data, size_t data_len){
-    CCHmacContext *ctx = hmac_context;
-    CCHmacUpdate(ctx, data, data_len);
-    return 0;
+    HMAC_CTX *ctx = hmac_context;
+    int result = HMAC_Update(ctx, data, data_len);
+    return (result == 1) ? 0 : -1;
 }
 
 int signal_hmac_sha256_final(signal_context *context, void *hmac_context, signal_buffer **output){
-    CCHmacContext *ctx = hmac_context;
+    int result = 0;
+    unsigned char md[EVP_MAX_MD_SIZE];
+    unsigned int len = 0;
+    HMAC_CTX *ctx = hmac_context;
 
-    signal_buffer *output_buffer = signal_buffer_alloc(CC_SHA256_DIGEST_LENGTH);
-    if(!output_buffer) {
-        return SG_ERR_NOMEM;
+    if(HMAC_Final(ctx, md, &len) != 1) {
+        return SG_ERR_UNKNOWN;
     }
 
-    CCHmacFinal(ctx, signal_buffer_data(output_buffer));
+    signal_buffer *output_buffer = signal_buffer_create(md, len);
+    if(!output_buffer) {
+        result = SG_ERR_NOMEM;
+        goto complete;
+    }
 
     *output = output_buffer;
 
-    return 0;
+complete:
+    return result;
 }
 
 void signal_hmac_sha256_cleanup(signal_context *context, void *hmac_context){
     if(hmac_context) {
-        CCHmacContext *ctx = hmac_context;
+        HMAC_CTX *ctx = hmac_context;
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+        HMAC_CTX_free(ctx);
+#else
+        HMAC_CTX_cleanup(ctx);
         free(ctx);
+#endif
     }
+}
+
+const EVP_CIPHER *aes_cipher(int cipher, size_t key_len)
+{
+    if(cipher == SG_CIPHER_AES_CBC_PKCS5) {
+        if(key_len == 16) {
+            return EVP_aes_128_cbc();
+        }
+        else if(key_len == 24) {
+            return EVP_aes_192_cbc();
+        }
+        else if(key_len == 32) {
+            return EVP_aes_256_cbc();
+        }
+    }
+    else if(cipher == SG_CIPHER_AES_CTR_NOPADDING) {
+        if(key_len == 16) {
+            return EVP_aes_128_ctr();
+        }
+        else if(key_len == 24) {
+            return EVP_aes_192_ctr();
+        }
+        else if(key_len == 32) {
+            return EVP_aes_256_ctr();
+        }
+    }
+    return 0;
 }
 
 int signal_sha512_digest_init(signal_context *context, void **digest_context){
     int result = 0;
+    EVP_MD_CTX *ctx;
 
-    CC_SHA512_CTX *ctx = malloc(sizeof(CC_SHA512_CTX));
+    ctx = EVP_MD_CTX_create();
     if(!ctx) {
         result = SG_ERR_NOMEM;
         goto complete;
     }
 
-    result = CC_SHA512_Init(ctx);
-    if(result != 1) {
+    result = EVP_DigestInit_ex(ctx, EVP_sha512(), 0);
+    if(result == 1) {
+        result = SG_SUCCESS;
+    }
+    else {
         result = SG_ERR_UNKNOWN;
-        goto complete;
     }
 
 complete:
     if(result < 0) {
         if(ctx) {
-            free(ctx);
+            EVP_MD_CTX_destroy(ctx);
         }
     }
     else {
@@ -107,19 +140,20 @@ complete:
 }
 
 int signal_sha512_digest_update(signal_context *context, void *digest_context, const uint8_t *data, size_t data_len){
-    CC_SHA512_CTX *ctx = digest_context;
+    EVP_MD_CTX *ctx = digest_context;
 
-    int result = CC_SHA512_Update(ctx, data, data_len);
+    int result = EVP_DigestUpdate(ctx, data, data_len);
 
     return (result == 1) ? SG_SUCCESS : SG_ERR_UNKNOWN;
 }
 
 int signal_sha512_digest_final(signal_context *context, void *digest_context, signal_buffer **output){
     int result = 0;
-    unsigned char md[CC_SHA512_DIGEST_LENGTH];
-    CC_SHA512_CTX *ctx = digest_context;
+    unsigned char md[EVP_MAX_MD_SIZE];
+    unsigned int len = 0;
+    EVP_MD_CTX *ctx = digest_context;
 
-    result = CC_SHA512_Final(md, ctx);
+    result = EVP_DigestFinal_ex(ctx, md, &len);
     if(result == 1) {
         result = SG_SUCCESS;
     }
@@ -128,7 +162,7 @@ int signal_sha512_digest_final(signal_context *context, void *digest_context, si
         goto complete;
     }
 
-    result = CC_SHA512_Init(ctx);
+    result = EVP_DigestInit_ex(ctx, EVP_sha512(), 0);
     if(result == 1) {
         result = SG_SUCCESS;
     }
@@ -137,7 +171,7 @@ int signal_sha512_digest_final(signal_context *context, void *digest_context, si
         goto complete;
     }
 
-    signal_buffer *output_buffer = signal_buffer_create(md, CC_SHA512_DIGEST_LENGTH);
+    signal_buffer *output_buffer = signal_buffer_create(md, len);
     if(!output_buffer) {
         result = SG_ERR_NOMEM;
         goto complete;
@@ -150,32 +184,8 @@ complete:
 }
 
 void signal_sha512_digest_cleanup(signal_context *context, void *digest_context){
-    if(digest_context) {
-        CC_SHA512_CTX *ctx = digest_context;
-        free(ctx);
-    }
-}
-
-int cc_status_to_result(CCCryptorStatus status)
-{
-    switch(status) {
-    case kCCSuccess:
-        return SG_SUCCESS;
-    case kCCParamError:
-    case kCCBufferTooSmall:
-        return SG_ERR_INVAL;
-    case kCCMemoryFailure:
-        return SG_ERR_NOMEM;
-    case kCCAlignmentError:
-    case kCCDecodeError:
-    case kCCUnimplemented:
-    case kCCOverflow:
-    case kCCRNGFailure:
-    case kCCUnspecifiedError:
-    case kCCCallSequenceError:
-    default:
-        return SG_ERR_UNKNOWN;
-    }
+    EVP_MD_CTX *ctx = digest_context;
+    EVP_MD_CTX_destroy(ctx);
 }
 
 int signal_encrypt(signal_context *context,
@@ -186,58 +196,90 @@ int signal_encrypt(signal_context *context,
         const uint8_t *plaintext, size_t plaintext_len)
 {
     int result = 0;
+    EVP_CIPHER_CTX *ctx = 0;
     uint8_t *out_buf = 0;
-    CCCryptorStatus status = kCCSuccess;
-    CCCryptorRef ref = 0;
 
-    if(cipher == SG_CIPHER_AES_CBC_PKCS5) {
-        status = CCCryptorCreate(kCCEncrypt, kCCAlgorithmAES, kCCOptionPKCS7Padding, key, key_len, iv, &ref);
+    const EVP_CIPHER *evp_cipher = aes_cipher(cipher, key_len);
+    if(!evp_cipher) {
+        fprintf(stderr, "invalid AES mode or key size: %zu\n", key_len);
+        return SG_ERR_UNKNOWN;
     }
-    else if(cipher == SG_CIPHER_AES_CTR_NOPADDING) {
-        status = CCCryptorCreateWithMode(kCCEncrypt, kCCModeCTR, kCCAlgorithmAES, ccNoPadding,
-                iv, key, key_len, 0, 0, 0, kCCModeOptionCTR_BE, &ref);
+
+    if(iv_len != 16) {
+        fprintf(stderr, "invalid AES IV size: %zu\n", iv_len);
+        return SG_ERR_UNKNOWN;
     }
-    else {
-        status = kCCParamError;
+
+    if(plaintext_len > INT_MAX - EVP_CIPHER_block_size(evp_cipher)) {
+        fprintf(stderr, "invalid plaintext length: %zu\n", plaintext_len);
+        return SG_ERR_UNKNOWN;
     }
-    if(status != kCCSuccess) {
-        result = cc_status_to_result(status);
+
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+    ctx = EVP_CIPHER_CTX_new();
+    if(!ctx) {
+        result = SG_ERR_NOMEM;
+        goto complete;
+    }
+#else
+    ctx = malloc(sizeof(EVP_CIPHER_CTX));
+    if(!ctx) {
+        result = SG_ERR_NOMEM;
+        goto complete;
+    }
+    EVP_CIPHER_CTX_init(ctx);
+#endif
+
+    result = EVP_EncryptInit_ex(ctx, evp_cipher, 0, key, iv);
+    if(!result) {
+        fprintf(stderr, "cannot initialize cipher\n");
+        result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    size_t available_len = CCCryptorGetOutputLength(ref, plaintext_len, 1);
-    out_buf = malloc(available_len);
+    if(cipher == SG_CIPHER_AES_CTR_NOPADDING) {
+        result = EVP_CIPHER_CTX_set_padding(ctx, 0);
+        if(!result) {
+            fprintf(stderr, "cannot set padding\n");
+            result = SG_ERR_UNKNOWN;
+            goto complete;
+        }
+    }
+
+    out_buf = malloc(sizeof(uint8_t) * (plaintext_len + EVP_CIPHER_block_size(evp_cipher)));
     if(!out_buf) {
         fprintf(stderr, "cannot allocate output buffer\n");
         result = SG_ERR_NOMEM;
         goto complete;
     }
 
-    size_t update_moved_len = 0;
-    status = CCCryptorUpdate(ref, plaintext, plaintext_len, out_buf, available_len, &update_moved_len);
-    if(status != kCCSuccess) {
-        result = cc_status_to_result(status);
+    int out_len = 0;
+    result = EVP_EncryptUpdate(ctx,
+        out_buf, &out_len, plaintext, plaintext_len);
+    if(!result) {
+        fprintf(stderr, "cannot encrypt plaintext\n");
+        result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    size_t final_moved_len = 0;
-    status = CCCryptorFinal(ref, out_buf + update_moved_len, available_len - update_moved_len, &final_moved_len);
-    if(status != kCCSuccess) {
-        result = cc_status_to_result(status);
+    int final_len = 0;
+    result = EVP_EncryptFinal_ex(ctx, out_buf + out_len, &final_len);
+    if(!result) {
+        fprintf(stderr, "cannot finish encrypting plaintext\n");
+        result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    signal_buffer *output_buffer = signal_buffer_create(out_buf, update_moved_len + final_moved_len);
-    if(!output_buffer) {
-        result = SG_ERR_NOMEM;
-        goto complete;
-    }
-
-    *output = output_buffer;
+    *output = signal_buffer_create(out_buf, out_len + final_len);
 
 complete:
-    if(ref) {
-        CCCryptorRelease(ref);
+    if(ctx) {
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+        EVP_CIPHER_CTX_free(ctx);
+#else
+        EVP_CIPHER_CTX_cleanup(ctx);
+        free(ctx);
+#endif
     }
     if(out_buf) {
         free(out_buf);
@@ -253,57 +295,90 @@ int signal_decrypt(signal_context *context,
         const uint8_t *ciphertext, size_t ciphertext_len)
 {
     int result = 0;
+    EVP_CIPHER_CTX *ctx = 0;
     uint8_t *out_buf = 0;
-    CCCryptorStatus status = kCCSuccess;
-    CCCryptorRef ref = 0;
 
-    if(cipher == SG_CIPHER_AES_CBC_PKCS5) {
-        status = CCCryptorCreate(kCCDecrypt, kCCAlgorithmAES, kCCOptionPKCS7Padding, key, key_len, iv, &ref);
+    const EVP_CIPHER *evp_cipher = aes_cipher(cipher, key_len);
+    if(!evp_cipher) {
+        fprintf(stderr, "invalid AES mode or key size: %zu\n", key_len);
+        return SG_ERR_INVAL;
     }
-    else if(cipher == SG_CIPHER_AES_CTR_NOPADDING) {
-        status = CCCryptorCreateWithMode(kCCDecrypt, kCCModeCTR, kCCAlgorithmAES, ccNoPadding,
-                iv, key, key_len, 0, 0, 0, kCCModeOptionCTR_BE, &ref);
+
+    if(iv_len != 16) {
+        fprintf(stderr, "invalid AES IV size: %zu\n", iv_len);
+        return SG_ERR_INVAL;
     }
-    else {
-        status = kCCParamError;
+
+    if(ciphertext_len > INT_MAX - EVP_CIPHER_block_size(evp_cipher)) {
+        fprintf(stderr, "invalid ciphertext length: %zu\n", ciphertext_len);
+        return SG_ERR_UNKNOWN;
     }
-    if(status != kCCSuccess) {
-        result = cc_status_to_result(status);
+
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+    ctx = EVP_CIPHER_CTX_new();
+    if(!ctx) {
+        result = SG_ERR_NOMEM;
+        goto complete;
+    }
+#else
+    ctx = malloc(sizeof(EVP_CIPHER_CTX));
+    if(!ctx) {
+        result = SG_ERR_NOMEM;
+        goto complete;
+    }
+    EVP_CIPHER_CTX_init(ctx);
+#endif
+
+    result = EVP_DecryptInit_ex(ctx, evp_cipher, 0, key, iv);
+    if(!result) {
+        fprintf(stderr, "cannot initialize cipher\n");
+        result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    out_buf = malloc(sizeof(uint8_t) * ciphertext_len);
+    if(cipher == SG_CIPHER_AES_CTR_NOPADDING) {
+        result = EVP_CIPHER_CTX_set_padding(ctx, 0);
+        if(!result) {
+            fprintf(stderr, "cannot set padding\n");
+            result = SG_ERR_UNKNOWN;
+            goto complete;
+        }
+    }
+
+    out_buf = malloc(sizeof(uint8_t) * (ciphertext_len + EVP_CIPHER_block_size(evp_cipher)));
     if(!out_buf) {
         fprintf(stderr, "cannot allocate output buffer\n");
         result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    size_t update_moved_len = 0;
-    status = CCCryptorUpdate(ref, ciphertext, ciphertext_len, out_buf, ciphertext_len, &update_moved_len);
-    if(status != kCCSuccess) {
-        result = cc_status_to_result(status);
+    int out_len = 0;
+    result = EVP_DecryptUpdate(ctx,
+        out_buf, &out_len, ciphertext, ciphertext_len);
+    if(!result) {
+        fprintf(stderr, "cannot decrypt ciphertext\n");
+        result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    size_t final_moved_len = 0;
-    status = CCCryptorFinal(ref, out_buf + update_moved_len, ciphertext_len - update_moved_len, &final_moved_len);
-    if(status != kCCSuccess) {
-        result = cc_status_to_result(status);
+    int final_len = 0;
+    result = EVP_DecryptFinal_ex(ctx, out_buf + out_len, &final_len);
+    if(!result) {
+        fprintf(stderr, "cannot finish decrypting ciphertext\n");
+        result = SG_ERR_UNKNOWN;
         goto complete;
     }
 
-    signal_buffer *output_buffer = signal_buffer_create(out_buf, update_moved_len + final_moved_len);
-    if(!output_buffer) {
-        result = SG_ERR_NOMEM;
-        goto complete;
-    }
-
-    *output = output_buffer;
+    *output = signal_buffer_create(out_buf, out_len + final_len);
 
 complete:
-    if(ref) {
-        CCCryptorRelease(ref);
+    if(ctx) {
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+        EVP_CIPHER_CTX_free(ctx);
+#else
+        EVP_CIPHER_CTX_cleanup(ctx);
+        free(ctx);
+#endif
     }
     if(out_buf) {
         free(out_buf);
