bf564da796cc520e6181b7ed1192049510bf57e9
Rizwan Asif <rizasif@gmail.com>
2018-02-15 12:58:55 +0500

    One Way: Irene Initialize

--- src/Codistan/one_way_simulation.c
+++ src/Codistan/one_way_simulation.c
@@ -175,7 +175,7 @@ int main(void)
     };
 
     printf("Initializaing Irene\n");
-    Initialize(&user_id_irene, &provider_irene, &global_context_irene);
+    Initialize(user_id_irene, provider_irene, global_context_irene);
     printf("Initializaing Irene Complete\n");
 
     // ClientInstall();
