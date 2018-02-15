19c8ad6339efc10f0d63ad50abeeefc58c16c3a4
Rizwan Asif <rizasif@gmail.com>
2018-02-15 13:38:45 +0500

    One Way: Irene Initialize

--- src/Codistan/one_way_simulation.c
+++ src/Codistan/one_way_simulation.c
@@ -181,8 +181,8 @@ int main(void)
     printf("Initializaing Irene\n");
     Initialize(user_id_irene, &provider_irene, global_context_irene);
 
-    printf("Installing Client Irene\n");
-    ClientInstall(identity_key_pair_irene, registration_id_irene, pre_keys_head_irene, signed_pre_key_irene, global_context_irene);
+    // printf("Installing Client Irene\n");
+    // ClientInstall(identity_key_pair_irene, registration_id_irene, pre_keys_head_irene, signed_pre_key_irene, global_context_irene);
 
     // GenerateKeys();
 
