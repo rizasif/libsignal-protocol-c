713d13d1ca309227339b908b2d100ae555452f24
Rizwan Asif <rizasif@gmail.com>
2018-02-15 13:27:52 +0500

    One Way: Irene Initialize

--- src/Codistan/one_way_simulation.c
+++ src/Codistan/one_way_simulation.c
@@ -75,7 +75,7 @@ unsigned long long getCurrentEpochTime(){
 
 /*Main Functions Start*/
 
-void Initialize(int user_id, 
+void Initialize(int *user_id, 
                 signal_crypto_provider *provider,
                 signal_context *global_context)
 {
@@ -179,7 +179,7 @@ int main(void)
     };
 
     printf("Initializaing Irene\n");
-    Initialize(user_id_irene, &provider_irene, global_context_irene);
+    Initialize(&user_id_irene, &provider_irene, global_context_irene);
 
     // printf("Installing Client Irene\n");
     // ClientInstall(identity_key_pair_irene, registration_id_irene, pre_keys_head_irene, signed_pre_key_irene);
